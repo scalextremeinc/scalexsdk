f494194c6b5ec683272399e4cc07f2c41812b9ee
cc <midnacc@gmail.com>
2012-06-07 16:33:19 +0800

    rewrite with new API

--- scalex_cmd.py
+++ scalex_cmd.py
@@ -9,11 +9,19 @@ import base64
 import sys
 import scalex
 import time
+#
+import scalex
 
 class scalex_cmd(cmd.Cmd):
   
-  scalex_instance = None;
-  
+  companies = []
+  roles = []
+  myscripts = []
+  orgscripts = []
+  scripts = []
+  nodes = []
+  jobs = []
+  runs = []
   def __init__ ( self ):
     self.prompt = "scalex>>";
     cmd.Cmd.__init__(self);
@@ -37,6 +45,7 @@ class scalex_cmd(cmd.Cmd):
       print "\tget"
       print "\tset"
       print "\tscript"
+      print "\tjob"
       print "\texit"
       print "help COMMAND for more information on a specific command."
     return 
@@ -45,24 +54,23 @@ class scalex_cmd(cmd.Cmd):
     print "login username password"
   
   def help_get(self):
-    print "get json [companies/roles/myScripts/orgScripts]=> get json format data"
     print "get companies => get companies"
     print "get roles => get roles"
     print "get nodes [nodename] => get nodes info, if nodename is provided, show the detail of this node"
-    print "get myScripts => get myScripts"
-    print "get orgScripts => get orgScripts"
-    print "get jobs [scriptId] => get jobs for script"
-    print "get runs [jobId] => get runs for jobs"
-    print "get arguments [scriptId] [script version] => get runs for jobs"
-    print "get output [jobId, projectId, projectRunId] => get output for runs"
+    print "get orgscripts => get org scripts"
+    print "get myscripts => get my scripts"
+    print "get arguments INDEX => get arguments for script"
+    print "get jobs INDEX => get jobs for script"
+    print "get runs INDEX => get runs for jobs"
+    print "get output INDEX => get output for runs"
 
   def help_set(self):
-    print 'set company [companyId]'
-    print 'set role [companyId]'
+    print 'set company INDEX'
+    print 'set role INDEX'
 
   def help_script(self):
-    print "script run [job name] [scriptId] [version] [targetIds] [startTime] [parameters]"
-    print 'targets are comma separated example: 101,102'
+    print 'script run script_INDEX node_INDEX,node_INDEX job_name [arguments]'
+    print 'targets are comma separated example: 1,2'
     print 'startTime 0 means run now, or schedule time format 2012-06-02-12:12'
     print 'parameters are space separated example: argument1 argument2'
   
@@ -78,23 +86,16 @@ class scalex_cmd(cmd.Cmd):
     password = l[1].strip();
     sx = None;
     try :
-      sx = scalex.scaleXtreme( username , password )
-      ret,value = sx.login();
-      if ( ret ):
-        sx.getCompanies()
-#        sx.getRoles()
-        self.scalex_instance = sx
+      ret = scalex.login( username , password )
+      if ret['result'] == 'SUCCESS':
         print "Login Successfully"
       else :
-        print "Login Failed:" + value
+        print "Login Failed: " + ret['data']
     except Exception,e :
       print "Login Failed:"
       print e
   
   def do_set(self, s):
-    if ( self.scalex_instance == None ):
-      print "Please Login first"
-      return 
     l = s.split()
     if len(l)!=2:
       self.help_set();
@@ -102,11 +103,12 @@ class scalex_cmd(cmd.Cmd):
     try :
       s = s.strip()
       param = s.split();
+      index = int(param[1])
       if ( param[0] == "company" ):
-        self.scalex_instance.setCompany(param[1]);
+        scalex.company.set(self.companies[index]);
         print 'set company ok'
       elif ( param[0] == "role" ):
-        self.scalex_instance.setRole(param[1]);
+        scalex.role.set(self.roles[index]);
         print 'set role ok'
       else :
         self.help_set();
@@ -114,118 +116,112 @@ class scalex_cmd(cmd.Cmd):
       print "Unknown Error:" , e
         
   def do_get(self,s):
-    
-    if ( self.scalex_instance == None ):
-      print "Please Login first"
-      return 
     try :
       s = s.strip()
       param = s.split();
-      if param[0] == 'json':
-        if len(param) != 2:
-          self.help_get()
-        elif param[1] == 'companies':
-          print json.dumps(self.scalex_instance.companies)
-        elif param[1] == 'roles':
-          print json.dumps(self.scalex_instance.roles)
-        elif param[1] == 'myScripts':
-          print json.dumps(self.scalex_instance.myScripts)
-        elif param[1] == 'orgScripts':
-          print json.dumps(self.scalex_instance.orgScripts)
-        else:
-          self.help_get()
-            
+      param[0] = param[0].lower()
       if ( param[0] == "companies" ):
-        self.scalex_instance.getCompanies();
-#        print self.scalex_instance.companies;
-        for com in self.scalex_instance.companies:
-          print 'name: %s \t\t companyId: ' % (com['name']), com['companyId']
-
+        coms = scalex.company.getCompanies()['data'];
+        self.companies = coms
+        for c in coms:
+          print 'index: [%d] %s' % (coms.index(c), c['name'])
       elif ( param[0] == "roles" ):
-        self.scalex_instance.getRoles();
-        print self.scalex_instance.roles;
-
-      elif ( param[0] == "myScripts" ):
-        print 'get my scripts'
-        self.scalex_instance.getMyScripts();
-        for s in self.scalex_instance.myScripts:
-          print 'id: %s\tversion: %s\tname: %s' % (s['scriptId'], s['version'], s['scriptName'])
-
-      elif ( param[0] == "orgScripts" ):
-        self.scalex_instance.getOrgScripts();
-        for s in self.scalex_instance.myScripts:
-          print 'id: %s\tversion: %s\tname: %s' % (s['scriptId'], s['version'], s['scriptName'])
+        roles = scalex.role.getRoles()['data'];
+        self.roles = roles
+        for i in roles:
+          print 'index: [%d] %s' % (roles.index(i), i)
+      elif ( param[0] == "myscripts" ):
+        scripts = scalex.script.getScripts()['data'];
+        self.scripts = scripts
+        for i in scripts:
+          print 'index: [%d] name: %s' % (scripts.index(i), i['scriptName'])
+      elif ( param[0] == "orgscripts" ):
+        scripts = scalex.script.getScripts(1)['data'];
+        self.scripts = scripts
+        for i in scripts:
+          print 'index: [%d] name: %s' % (scripts.index(i), i['scriptName'])
 
       elif ( param[0] == "jobs" ):
         if len(param) < 2:
           self.help_get() 
-        self.scalex_instance.getJobsForScript(param[1]);
-        for j in self.scalex_instance.jobs[param[1]]:
-          print 'jobId: %s\tstatus: %s\t\tjobName: %s' % (j['jobId'], j['status'], j['jobName'])
+        i = int(param[1])
+        script = self.scripts[i]
+        jobs = scalex.job.getJobs(script)['data'];
+        self.jobs = jobs
+        for i in jobs:
+          print 'index: [%d] name: %s' % (jobs.index(i), i['jobName'])
 
       elif ( param[0] == "runs" ):
         if len(param) < 2:
           self.help_get()
-        jobId = param[1]
-        self.scalex_instance.getRunsForJob(jobId);
-        for r in self.scalex_instance.runs[jobId]:
-          print 'jobId: %s\tprojectId: %s\tprojectRunId: %s\tstatus: %s\t' % (r['jobId'], r['projectId'], r['projectRunId'], r['status'])
+        i = int(param[1])
+        job = self.jobs[i]
+        self.runs = scalex.job.getRuns(job)['data'];
+        for i in self.runs:
+          print 'index: [%d] status: %s task name: %s' % (self.runs.index(i), i['status'], i[u'stepRunLogBeans'][0]['taskName'])
 
       elif param[0] == 'arguments':
-        if len(param) < 3:
+        if len(param) < 2:
           self.help_get()
-        scriptId = param[1]
-        version = param[2]
-        self.scalex_instance.getParamsOfScript(scriptId, version)
-        print 'script: %s, scriptId: %s' % (self.scalex_instance.arguments[scriptId]['scriptName'], self.scalex_instance.arguments[scriptId]['scriptId'])
-        for arg in self.scalex_instance.arguments[scriptId]['scriptInputParams']:
+        index = int(param[1])
+        script = self.scripts[index]
+        content = scalex.script.getContent(script)['data']
+        print 'script: %s' % (content['scriptName'])
+        for arg in content['scriptInputParams']:
           print 'argu: %s\ttype: %s\tdefault value: %s' % (arg['parameterKey'],arg['parameterDataType'],arg['parameterDefaultValue'])
  
       elif ( param[0] == "output" ):
-        if len(param) != 4:
+        if len(param) < 2:
           self.help_get()
-        jobId = param[1]
-        projectId = param[2]
-        projectRunId = param[3]
-        self.scalex_instance.getOutputForRun(jobId, projectId, projectRunId)
+        i = int(param[1])
+        run = self.runs[i]
+        outputs  = scalex.job.getOutputs(run)['data']
         
-        for r in self.scalex_instance.runs[jobId]:
-          print 'jobname:', r['stepRunLogBeans'][0]['taskName']
+        for r in outputs:
+          print 'jobname:', run['stepRunLogBeans'][0]['taskName']
           print 'status: ', r['status']
-          print 'run at: ', time.ctime(int(r['runTimestamp'])/1000)
-        
-        print '-----------------'  
-        for output in self.scalex_instance.outputs:
-            print 'target:', output['target']
-            print 'outputStatus:', output['outputStatus']
-            if output['truncated'] == 'Y' :
-                print 'output (truncated - more than 500 chars):'
-            else: 
-                print 'output:'
-            print output['output'] 
-            print '-----------------'
+          print 'output: ', base64.b64decode(r['output'])
+          print 'run at: ', time.ctime(int(run['runTimestamp'])/1000)
+#        print '-----------------'  
+#        for output in self.outputs:
+#            print 'target:', output['target']
+#            print 'outputStatus:', output['outputStatus']
+#            if output['truncated'] == 'Y' :
+#                print 'output (truncated - more than 500 chars):'
+#            else: 
+#                print 'output:'
+#            print output['output'] 
+#            print '-----------------'
           
       elif ( param[0] == "nodes" ):
-        self.scalex_instance.getNodes()
-        for n in self.scalex_instance.nodes:
-          print 'nodeId: %d\tnodeName: %s' % (n['nodeId'], n['nodeName'])
-          
+        self.nodes = scalex.node.getNodes()['data']
+        for n in self.nodes:
+          print 'index:[%d] nodeName: %s' % (self.nodes.index(n), n['nodeName'])
     except Exception,e:
       print "Unknown Error:" , e
   
   def do_script(self,s):
-    
-    if ( self.scalex_instance == None ):
-      print "Please Login first"
-      return 
     try :
       param = s.split()
-      if len(param) < 6:
+      if len(param) < 4:
         self.help_script()
         return
       if ( param[0] == "run" ):
-        del param[0]
-        self.scalex_instance.runScript(param);
+        script = self.scripts[int(param[1])]
+        targets = []
+        for i in param[2].split(','):
+          targets.append(self.nodes[int(i)])
+        name = param[3]
+        arguments = []
+        try:
+          arguments = param[4].split(',')
+        except:
+          pass
+#        scheduleType = int(param[5])
+#        #schedule type should be one of 0 1 2
+#        startTime = param[6]
+        result = scalex.script.run(name, script, targets, arguments = arguments)
+        print result['result']
     except Exception,e:
       print "Unknown Error:" , e
   
@@ -237,7 +233,5 @@ class scalex_cmd(cmd.Cmd):
     sys.exit(0);
 
 if __name__ == '__main__':
-  
   scalex_cmd().cmdloop();
-  
   sys.exit(0);
